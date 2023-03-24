5deb3bb1506ee200791091e291fd2ca5db38b26f
Junior Jean <44121748+L33t-Rose@users.noreply.github.com>
2023-03-24 03:25:04 +0000

    Added time limit parameter

--- Cache.ts
+++ Cache.ts
@@ -1,25 +1,77 @@
+type timeUnits = 'd' | 'h' | 'm' | 's' | 'ms';
 export class Cache<T>{
-  cacheMap: Map<string, { data: T, invalidate: void }>;
+  #cacheMap: Map<string, { data: T, _invalidate: void }>;
   constructor() {
-    this.cacheMap = new Map();
+    this.#cacheMap = new Map();
   }
   get(key: string) {
-    let res = this.cacheMap.get(key);
+    let res = this.#cacheMap.get(key);
     if (!res) {
       return null;
     }
     return res.data;
   }
 
-  set(key: string, data: T) {
-    this.cacheMap.set(key, {
+  private stringToTimelimit(input: string[]): number {
+    if (!input) {
+      throw new Error("Invalid input");
+    }
+    let second = 1000;
+    let minute = 60 * second;
+    let hour = 60 * minute;
+    let day = 24 * hour;
+
+    let sum = 0;
+    for (let time of input) {
+      let unit = time.split(/\d/i).pop();
+      if (!unit || ['d', 'h', 'm', 's', 'ms'].indexOf(unit) == -1) {
+        throw new Error("Invalid Unit of Time");
+      }
+      let measure = parseInt(time.split(unit)[0]);
+      if (isNaN(measure)) { throw new Error("Not a number") }
+      switch (unit) {
+        case 'd':
+          sum += measure * day;
+          break;
+        case 'h':
+          sum += measure * hour;
+          break;
+        case 'm':
+          sum += measure * minute;
+          break;
+        case 's':
+          sum += measure * second;
+          break;
+        case 'ms':
+          sum += measure;
+          break;
+        default:
+          console.log("ERROR");
+      }
+    }
+    return sum;
+  }
+  
+  set(key: string, data: T, timeLimit: number | Array<`${number}${timeUnits}`> | "infinite" = 60 * 1000) {
+    let time: number;
+    if (Array.isArray(timeLimit)) {
+      time = this.stringToTimelimit(timeLimit);
+    }
+    else if (timeLimit == "infinite") { time = 0; }
+    else { time = timeLimit; }
+    
+    this.#cacheMap.set(key, {
       data: data,
-      invalidate: (() => {
+      _invalidate: (() => {
+        if (typeof timeLimit == "string" && timeLimit === "infinite") {
+          return;
+        }
+        console.log("timeLimit", time);
         let c = setTimeout(() => {
           console.log("cache invalidated");
-          this.cacheMap.delete(key);
+          this.#cacheMap.delete(key);
           clearTimeout(c);
-        }, 10 * 1000);
+        }, time);
       })()
     })
   }
--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
 import { Cache } from './Cache';
 const test = new Cache<unknown>();
 
-const fetch = (url: RequestInfo, init?: RequestInit) =>  import("node-fetch").then(({ default: fetch }) => fetch(url, init));
+//Took this from somewhere to get fetch working in 
+const fetch = (url: RequestInfo, init?: RequestInit) => import("node-fetch").then(({ default: fetch }) => fetch(url, init));
 
 async function fetching() {
   let res = test.get("pikachu");
   if (res == null) {
     console.log("fetching from pokeAPI");
     res = await (await fetch("https://pokeapi.co/api/v2/pokemon/ditto")).json();
-    test.set("pikachu", res);
+    test.set("pikachu", res, ["1h", "10m"]);
+  } else {
+    console.log("grabbing from cache");
   }
   return res;
 }
 
 let a = setInterval(async () => {
   const res = await fetching();
-  console.log(res);
+  //console.log(res);
 }, 1000);
